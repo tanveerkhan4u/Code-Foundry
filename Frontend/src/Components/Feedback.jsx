a3fdaf7d4bb42bb4b6f8fca2b98cf747afaa4fc2
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-02-11 11:29:49 +0530

    update

--- Frontend/src/Components/Feedback.jsx
+++ Frontend/src/Components/Feedback.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { enqueueSnackbar } from 'notistack';
+import { useNavigate } from 'react-router-dom';
+
 
 const Feedback = () => {
   return (
     <div>
       
-<div className='container-fluid d-flex py-5' style={{background:"#dbedff", height:"400px"}}>
-  <img src=" https://thumbs.dreamstime.com/b/feedback-concept-image-arrows-blue-chalkboard-background-40378284.jpg"   style={{width:"400px", height:"250px", marginLeft:"150px"}}    alt="" />
-<h1 className='text ' style={{color:"red", marginLeft:"200px"}}><strong>Your Feedback Matters </strong></h1>
-<p className='text'>Please provide your feedback so that we can continue to improve our service.
-Full name:
-</p>
+  
+  
+<div className='container-fluid' style={{backgroundColor:"#dbedff"}}><br/><br/>
+<h1 className='text' style={{color:"red", marginLeft:"400px"}}><strong>Your Feedback Matters </strong></h1>
+<p className='text-center mt-4' style={{ color:"grey", fontSize:"23px"}}>Please provide your feedback so that we can continue to improve our service.
+</p><br/>
+<form>
+     <div className='form-group mt-4'>
+          <label htmlFor="Name"></label>
+    
+          <input type="text" className='form-control shadow' style={{width:"570px", height:"60px", marginLeft:"350px", marginTop:"-30px"}} placeholder='Your Name' id='name'  />
+          <label htmlFor="Email"></label>
+  
+          <input type="text" className='form-control shadow' style={{width:"570px", height:"60px", marginLeft:"350px" }}  placeholder='Your Email' id='email'  />
+          <label htmlFor="Message"></label>
+          <textarea id="message" className='form-control  shadow' style={{width:"570px", height:"180px", marginLeft:"350px" }}  placeholder='Your Message'   /><br/>
+          <button className='btn btn-primary shadow' style={{marginLeft:"350px", height:"60px", width:"570px", fontSize:"22px"}} type='submit'>Submit</button>
+          </div>
+     </form>
+<br/> 
+<br/><br/><br/>
 
-<br/>
 </div>
+
+    </div>
+  )
+}
+
+export default Feedback
+     
+
+
+
+
+
 
 
 
@@ -21,8 +52,3 @@ Full name:
 
 
       
-    </div>
-  )
-}
-
-export default Feedback
