ba1fd87e99368223b7d8f6d89d2aadd056d8ce1e
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-02-16 14:26:07 +0530

    update

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import { useAuth0 } from "@auth0/auth0-react";
 import { enqueueSnackbar } from 'notistack';
 import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion'
 
 
 const SignupSchema = Yup.object().shape({
@@ -22,8 +23,8 @@ const Login = () => {
       email: '',
       password: ''
     },
-    onSubmit: async(values, {setSubmitting, resetForm}) => { 
-      console.log(values); 
+    onSubmit: async (values, { setSubmitting, resetForm }) => {
+      console.log(values);
       setSubmitting(true);
       const res = await fetch('http://localhost:5000/user/authenticate', {
         method: 'POST',
@@ -34,14 +35,14 @@ const Login = () => {
       }
       );
       setSubmitting(false);
-    
+
       if (res.status === 200) {
         enqueueSnackbar('Logged In Successfully', { variant: 'success' });
 
         const data = await res.json();
 
         sessionStorage.setItem('user', JSON.stringify(data));
-        
+
         resetForm();
         navigate('/Home');
 
@@ -51,78 +52,63 @@ const Login = () => {
         enqueueSnackbar('Something went werong ', { variant: 'error' });
       }
     },
-      validationSchema: SignupSchema
-    });
-      
+    validationSchema: SignupSchema
+  });
 
-    const { user, loginWithRedirect } = useAuth0();
-    // console.log( user);
-  
-  
-  
-    return (
-      <div className='col-6 d-flex' style={{ backgroundColor: "#dbedff" }}>
-        <div>
-        <img src='https://codedthemes.com/wp-content/uploads/2022/04/sign-in-left-img-1.png.webp'  alt='' />
-        </div>
-      <div className='continer'style={{marginLeft:"50px"}}>
+
+  const { user, loginWithRedirect } = useAuth0();
+  // console.log( user);
+
+
+
+  return (
+
+    <div className='col-6 d-flex' style={{ backgroundColor: "#dbedff" }}>
+
+      <motion.img animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} src='https://codedthemes.com/wp-content/uploads/2022/04/sign-in-left-img-1.png.webp' className='img-fluid mx-auto' alt='' />
+
+      <div className='container-fluid' style={{ marginLeft: "30px" }}>
         <div className=' py-5'>
           <div className="container-fluid">
-    
-        
-                
-                <br/><br/><br/>
-                <form onSubmit={Loginform.handleSubmit}>
-                  <div className='container-fluid d-flex'>
-                <button className='btn btn-outline-dark shadow ' onClick={(e) => loginWithRedirect()} style={{marginTop:"-70px", width:"250px", height:"60px",marginLeft:"-10px"}}><i class="fa-brands fa-google"></i> &nbsp; Sign in With Google</button><br/><br/>
-                <button className='btn btn-outline-primary shadow ' onClick={(e) => loginWithRedirect()} style={{marginTop:"-70px", marginLeft:"20px" , width:"250px", height:"60px"}}><i class="fa-brands fa-linkedin"></i> &nbsp; Sign in With LinkedIn</button>
-                </div>
-                <h3 className='text-center' style={{color:"grey", marginTop:"-30px"}}>OR</h3><br/>
-                <p className='text-center'style={{fontSize:"25px"}}>Log into Your Account</p>
 
-                  <label htmlFor="" style={{fontSize:"20px"}}>Email </label>
-                  <span className='ms-4 fs-6 text-danger'>{Loginform.errors.email}</span>
-                  <input type="text" className='form-control mb-4' id='email'  style={{width:"530px",height:"50px"}} onChange={Loginform.handleChange} value={Loginform.values.email} />
-                  <label htmlFor="" style={{fontSize:"20px"}}>Password</label>
-                  <span className='ms-4 fs-6 text-danger'>{Loginform.errors.password}</span>
-                  <input type="password" className='form-control mb-4' id='password' style={{width:"530px",height:"50px"}} onChange={Loginform.handleChange} value={Loginform.values.password} /><br/>
 
-                  <input type='Checkbox' value='IsRemember me' id='rememberme'></input> 
-                  <label htmlFor='' style={{marginTop:"-20px",marginLeft:"10px"}}> Remember me</label>
-                  <button className='btn btn-outline-primary' style={{width:"530px", height:"50px",marginTop:"10px"}} type='submit' onClick="Isremember me">Log In</button>;
-                  <p className='text-center' style={{marginTop:"20px"}}>Don't have an account? <a href='/Signup'>Sign Up</a></p>
-                  &nbsp;
-                  <br />
-                </form>
+
+            <br /><br /><br />
+            <form onSubmit={Loginform.handleSubmit}>
+              <div className='container-fluid d-flex'>
+                <button className='btn btn-outline-dark shadow ' onClick={(e) => loginWithRedirect()} style={{ marginTop: "-70px", width: "250px", height: "60px", marginLeft: "-10px" }}><i class="fa-brands fa-google"></i> &nbsp; Sign in With Google</button><br /><br />
+                <button className='btn btn-outline-primary shadow ' onClick={(e) => loginWithRedirect()} style={{ marginTop: "-70px", marginLeft: "20px", width: "250px", height: "60px" }}><i class="fa-brands fa-linkedin"></i> &nbsp; Sign in With LinkedIn</button>
+              </div>
+              <h3 className='text-center' style={{ color: "grey", marginTop: "-30px" }}>OR</h3><br />
+              <p className='text-center' style={{ fontSize: "25px" }}>Log into Your Account</p>
+
+              <label htmlFor="" style={{ fontSize: "20px" }}>Email </label>
+              <span className='ms-4 fs-6 text-danger'>{Loginform.errors.email}</span>
+              <input type="text" className='form-control mb-4' id='email' onChange={Loginform.handleChange} value={Loginform.values.email} />
+              <label htmlFor="" style={{ fontSize: "20px" }}>Password</label>
+              <span className='ms-4 fs-6 text-danger'>{Loginform.errors.password}</span>
+              <input type="password" className='form-control mb-4' id='password' onChange={Loginform.handleChange} value={Loginform.values.password} /><br />
+
+              <input type='Checkbox' value='IsRemember me' className='mb-3' id='rememberme'></input>
+              <label htmlFor='' style={{ marginTop: "-20px", marginLeft: "10px" }}> Remember me</label>
+
+
+              <button className='btn btn-outline-primary w-100' type='submit' onClick="Isremember me">Log In</button>;
+              <p className='text-center' style={{ marginTop: "20px" }}>Don't have an account? <a href='/Signup'>Sign Up</a></p>
+              &nbsp;
+              <br />
+            </form>
           </div>
           <div className="col-md-9">
-  
+
           </div>
         </div>
-        </div>
       </div>
-    )
-  }
-  
-  export default Login;
+    </div>
+  )
+}
 
-                
-
-
-                    
-                    
-                  
-
-  
-                  
-                
-            
-
-
-  
-  
-              
-            
+export default Login;
 
 
 
@@ -131,15 +117,33 @@ const Login = () => {
 
 
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
