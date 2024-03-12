a94d8f10dcbba6d76793c5b9792d2f2294909f9b
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-12 16:55:04 +0530

    update

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -153,31 +153,31 @@ const Navbar = () => {
       <div className="sm:hidden" id="mobile-menu">
         <div className="space-y-1 px-2 pb-3 pt-2">
           {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-          <a
-            href="#"
+          <NavLink
+            to="/Home"
             className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
             aria-current="page"
           >
-            Dashboard
-          </a>
-          <a
-            href="#"
+            Home
+          </NavLink>
+          <NavLink
+            to="/Browsestack"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
-            Team
-          </a>
-          <a
-            href="#"
+            Browse Stack
+          </NavLink>
+          <NavLink
+            to="/Contact"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
-            Projects
-          </a>
-          <a
-            href="#"
+            Contact Us
+          </NavLink>
+          <NavLink
+            to="/Feedback"
             className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
           >
-            Calendar
-          </a>
+            Feedback
+          </NavLink>
         </div>
       </div>
     </nav>
