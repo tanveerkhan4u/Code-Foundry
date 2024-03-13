611245d457c82f10c4821011b1e4cd3b03f32a57
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-13 12:16:32 +0530

    update

--- Frontend/src/Components/Log.jsx
+++ Frontend/src/Components/Log.jsx
@@ -16,7 +16,7 @@ const  SignupFormDemo=()=> {
     console.log("Form submitted");
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black  ">
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to Aceternity
       </h2>
@@ -84,16 +84,9 @@ const  SignupFormDemo=()=> {
             </span>
             <BottomGradient />
           </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
-            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-              OnlyFans
-            </span>
-            <BottomGradient />
-          </button>
+          
+            
+      
         </div>
       </form>
     </div>
