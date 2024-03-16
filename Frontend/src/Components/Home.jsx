47c561e105eec8b6c4ae892afbe092aa3e518920
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-16 23:33:55 +0530

    update

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -135,15 +135,15 @@ const Home = () => {
 
         </div>
       </header>
-
-
-
-
       <br /><br />
-
       <div className='container'>
         <h3 className='text-center  text-gray-500 text-2xl mb-4'>We create custom for the application using the following technology</h3>
       </div>
+
+
+
+
+
 
 
       <div className='logo'>
@@ -369,20 +369,6 @@ const Home = () => {
         }
       </div>
      </div>
-
-
-
-
-  
-
-
-
-
-
-      
-      
-
-          
       <div className='container bg-body-secondary h-32 mb-5'>
         <marquee behavior="scroll" direction="left" scrollamount="15">
           <h1 className='text-center p-5 text-4xl  font-semibold'>MOBILE APPLICATION</h1>
@@ -539,8 +525,6 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-
-
       <div className="grid">
         <Movingcard />
       </div>
@@ -576,6 +560,22 @@ export default Home
 
 
 
+  
+
+
+
+
+
+      
+      
+
+          
+
+
+
+
+
+
 
 
 
@@ -595,59 +595,6 @@ export default Home
 
 
 
-//   <div className='container '>
-//   <div className='row gy-4 justify-content-between justify-content-between'>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className=' card col-md-3  shadow '>
-//       <img className='img-fluid mx-auto ' src='https://codedthemes.com/wp-content/uploads/2022/04/Flexible-code-icon-1.png.webp' style={{ marginLeft: "180px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center' style={{ marginTop: "20px" }}>Flexible Code</h4><br />
-//       <p className='text-center mb-5 mx-auto' style={{ color: "grey" }}>Easy to start your project with our easy to use code standard.</p>
-//     </motion.div>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className='card col-md-3  shadow '>
-//       <img className='img-fluid mx-auto  ' src='https://codedthemes.com/wp-content/uploads/2022/05/quality.svg' style={{ marginLeft: "180px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center mt-3' >Quality First</h4><br />
-//       <p className='text-center mb-5 mx-auto' style={{ color: "grey" }}>We believe in quality designs and code instead of quantity</p>
-//     </motion.div>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className='card col-md-3  shadow'>
-//       <img className='img-fluid mx-auto ' src='https://codedthemes.com/wp-content/uploads/2022/04/Freemium-Model-icon-1.png.webp' style={{ marginLeft: "40px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center' style={{ marginTop: "20px" }}>Freemium Model</h4><br />
-//       <p className='text-center px-3 mb-5' style={{ color: "grey" }}>Our belief on open-source led us to provide a free versions of most products</p>
-//     </motion.div>
-//   </div>
-// </div>
-// <br /><br />
-// <div className='container'>
-//   <div className='row gy-4 justify-content-between justify-content-between'>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className='card shadow col-md-3'>
-//       <img className='img-fluid mx-auto' src='https://codedthemes.com/wp-content/uploads/2022/04/Documentation-icon-1.png' style={{ marginLeft: "180px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center' style={{ marginTop: "20px" }}>Documentation</h4><br />
-//       <p className='text-center px-3' style={{ marginTop: "-20px", color: "grey" }}>Detailed step by step documentation helper file for each template</p>
-//     </motion.div>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className='card shadow col-md-3'>
-//       <img className='img-fluid mx-auto' src='https://codedthemes.com/wp-content/uploads/2022/04/Free-updates-icon-1.png' style={{ marginLeft: "180px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center' style={{ marginTop: "20px" }}>Free Updates</h4><br />
-//       <p className='text-center px-3' style={{ marginTop: "-20px", color: "grey" }}>Free updates help to keep your project always up to Date</p>
-//     </motion.div>
-//     <motion.div whileHover={{ scale: 1.2 }}
-//       onHoverStart={e => { }}
-//       onHoverEnd={e => { }} className='card shadow col-md-3'>
-//       <img className='img-fluid mx-auto' src='https://codedthemes.com/wp-content/uploads/2022/04/Support-icon-1.png' style={{ marginLeft: "180px", height: "40px", width: "40px", marginTop: "20px" }} alt='' />
-//       <h4 className='text-center' style={{ marginTop: "20px" }}>Support</h4><br />
-//       <p className='text-center px-3' style={{ marginTop: "-20px", color: "grey" }}>Support via chat & ticket to resolve your any technical query</p>
-//     </motion.div>
-
-//   </div>
-// </div>
-// <br /><br />
 
 
 
--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -67,7 +67,7 @@ const Login = () => {
 
 
 
-  const googleSignup= async (credentialResponse) => {
+  const googleSignup = async (credentialResponse) => {
 
     const emailRes = await fetch(`${import.meta.env.VITE_API_URL}/user/getbyemail/${credentialResponse.email}`);
 
@@ -76,7 +76,7 @@ const Login = () => {
       const userData = await emailRes.json();
       enqueueSnackbar('Loggedin Successfully ', { variant: 'success' });
       sessionStorage.setItem('user', JSON.stringify(userData));
-      setuserloggedIn(true);
+      setloggedIn(true);
       navigate('/')
     } else {
 
@@ -96,8 +96,8 @@ const Login = () => {
         enqueueSnackbar('Registered Successfully ', { variant: 'success' });
         const data = await res.json();
         sessionStorage.setItem('user', JSON.stringify(data));
-        setuserloggedIn(true);
-        navigate('/')
+        setloggedIn(true);
+        navigate('/Home')
       } else {
         enqueueSnackbar('Something went wrong', { variant: 'error' });
       }
@@ -110,94 +110,109 @@ const Login = () => {
 
   return (
     <>
-    <form onSubmit={Loginform.handleSubmit}>
-    <div className='flex mx-auto md:flex-row  flex-col bg-[#dbedff]'>
-      
-      <div className='basis-1/2 mb-5 '>
-        <motion.img  animate={{ x: 50, y: -10 }} transition={{ ease: "easeOut", duration: 2 }}  src='https://codedthemes.com/wp-content/uploads/2022/04/sign-in-left-img-1.png.webp' className='mt-5 mx-auto loimg' alt='' />
-      </div>
-      <div  className="flex h-screen w-screen items-center -mt-10">
-      
-  {/* Login */}
-  
-  <div className="relative flex w-96 flex-col space-y-5 rounded-lg border bg-white px-4 py-10 shadow-2xl sm:mx-auto">
-    <div className="-z-10 absolute top-4 left-1/2 h-full w-5/6 -translate-x-1/2 rounded-lg  sm:-right-10 sm:top-auto sm:left-auto sm:w-full sm:translate-x-0" />
-    <div className="mx-auto mb-2 space-y-3">
-      <h1 className="text-center text-3xl font-bold text-gray-700">Sign in</h1>
-      <p className="text-gray-500">Sign in to access your account</p>
-    </div>
-    
-    <div>
-      <div className="relative mt-2 w-full">
-      
-        <input
-          type="text"
-          id="email"
-          defaultValue=""
-          className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-          placeholder=" "
-          onChange={Loginform.handleChange} value={Loginform.values.email}
-        />
-        <label
-          htmlFor="email"
-          className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
-        >
-          {" "}
-          Enter Your Email{" "}
-        </label>
-        <span className=' text-danger'>{Loginform.touched.email && Loginform.errors.email}</span>
-      </div>
-    </div>
-    <div>
-      <div className="relative mt-2 w-full">
-        <input
-          type="password"
-          id="password"
-          className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-          placeholder=" ••••••••"
-          onChange={Loginform.handleChange} value={Loginform.values.password}
-        />
-        <label
-          htmlFor="password"
-          className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
-        >
-          {" "}
-          Enter Your Password
-        </label>
-        <span className=' text-danger'>{Loginform.touched.password && Loginform.errors.password}</span>
-      </div>
-    </div>
-    <div className="flex w-full items-center">
-      <button  type='submit' className="shrink-0 inline-block w-36 rounded-lg bg-blue-600 py-3 font-bold text-white">
-        Login
-      </button>
-      <a
-        className="w-full text-center text-sm font-medium text-gray-600 hover:underline"
-        href="#"
-      >
-        Forgot your password?
-      </a>
-    </div>
-    
-    <p className="text-center text-gray-600">
-      Don't have an account?
-      <NavLink
-        to="/Signup"
-        className="whitespace-nowrap font-semibold text-gray-900 hover:underline ml-4"
-      >
-        Sign up
-      </NavLink>
-    </p>
-    
-  </div>
-  
-  {/* /Login */}
-  
-</div>
+      <form onSubmit={Loginform.handleSubmit}>
+        <div className='flex mx-auto md:flex-row  flex-col bg-[#dbedff]'>
 
-      </div> 
+          <div className='basis-1/2 mb-5 '>
+            <motion.img animate={{ x: 50, y: -10 }} transition={{ ease: "easeOut", duration: 2 }} src='https://codedthemes.com/wp-content/uploads/2022/04/sign-in-left-img-1.png.webp' className='mt-5 mx-auto loimg' alt='' />
+          </div>
+          <div className="flex h-screen w-screen items-center -mt-10">
+
+            {/* Login */}
+
+            <div className="relative flex w-96 flex-col space-y-5 rounded-lg border bg-white px-4 py-10 shadow-2xl sm:mx-auto">
+              <div className="-z-10 absolute top-4 left-1/2 h-full w-5/6 -translate-x-1/2 rounded-lg  sm:-right-10 sm:top-auto sm:left-auto sm:w-full sm:translate-x-0" />
+              <div className="mx-auto mb-2 space-y-3">
+                <h1 className="text-center text-3xl font-bold text-gray-700">Sign in</h1>
+                <p className="text-gray-500">Sign in to access your account</p>
+              </div>
+
+              <div>
+                <div className="relative mt-2 w-full">
+
+                  <input
+                    type="text"
+                    id="email"
+                    defaultValue=""
+                    className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                    onChange={Loginform.handleChange} value={Loginform.values.email}
+                  />
+                  <label
+                    htmlFor="email"
+                    className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
+                  >
+                    {" "}
+                    Enter Your Email{" "}
+                  </label>
+                  <span className=' text-danger'>{Loginform.touched.email && Loginform.errors.email}</span>
+                </div>
+              </div>
+              <div>
+                <div className="relative mt-2 w-full">
+                  <input
+                    type="password"
+                    id="password"
+                    className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                    placeholder=" ••••••••"
+                    onChange={Loginform.handleChange} value={Loginform.values.password}
+                  />
+                  <label
+                    htmlFor="password"
+                    className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
+                  >
+                    {" "}
+                    Enter Your Password
+                  </label>
+                  <span className=' text-danger'>{Loginform.touched.password && Loginform.errors.password}</span>
+                </div>
+              </div>
+              <div className='google'>
+                <GoogleOAuthProvider clientId="933596296606-mqbgnqrpol73pu5lr8pl06p1taahd745.apps.googleusercontent.com">
+                  <GoogleLogin
+                    type='button'
+                    onSuccess={credentialResponse => {
+                      const decoded = jwtDecode(credentialResponse.credential);
+                      console.log(decoded);
+                      googleSignup(decoded);
+                    }}
+                    onError={() => {
+                      console.log('Login Failed');
+                    }}
+                  />
+                </GoogleOAuthProvider>
+              </div>
+              <div className="flex w-full items-center">
+                <button type='submit' className="shrink-0 inline-block w-36 rounded-lg bg-blue-600 py-3 font-bold text-white">
+                  Login
+                </button>
+                <a
+                  className="w-full text-center text-sm font-medium text-gray-600 hover:underline"
+                  href="#"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+
+              <p className="text-center text-gray-600">
+                Don't have an account?
+                <NavLink
+                  to="/Signup"
+                  className="whitespace-nowrap font-semibold text-gray-900 hover:underline ml-4"
+                >
+                  Sign up
+                </NavLink>
+              </p>
+
+            </div>
+
+            {/* /Login */}
+
+          </div>
+
+        </div>
       </form>
-      
+
     </>
   )
 }
