642222a37e23415e9a806a77d00c551abdfb4eb0
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-18 11:36:08 +0530

    update

--- Frontend/src/Components/Signup.jsx
+++ Frontend/src/Components/Signup.jsx
@@ -107,16 +107,16 @@ const Signup = () => {
     <>
    <div className="flex mx-auto md:flex-row  flex-col min-h-screen  bg-[#dbedff]">
     <div className='basis-1/2'>
-      <img src='https://i0.wp.com/stackdigi.com/storage/2020/04/logo-design.png?w=351&ssl=1'  className='mx-auto mt-5'  alt=''/>
+      <img src='https://i0.wp.com/stackdigi.com/storage/2020/04/logo-design.png?w=351&ssl=1'  className='mx-auto mt-5 '  alt=''/>
     </div>
     <div className='basis-1/2'>
     <section className=" dark:bg-gray-900 ">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 -mt-20">
+  <div className="flex flex-col   px-6 py-8 mx-auto md:h-screen lg:py-0 mt-4 ">
    
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create and account
+        <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign up to your account
         </h1>
         <form>
           <div>
@@ -130,7 +130,7 @@ const Signup = () => {
               type="email"
               name="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-3"
               placeholder="name@company.com"
               required=""
             />
@@ -194,9 +194,9 @@ const Signup = () => {
           </div>
           <button
             type="submit"
-            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="w-full text-dark bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
-            Create an account
+            Sign Up
           </button>
           <p className="text-sm font-light text-gray-500 dark:text-gray-400">
             Already have an account?{" "}
