aa197d4d09b65ea5d45ef06bd86551c1a7136ee4
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-20 12:31:17 +0530

    update

--- Frontend/src/Components/Item.jsx
+++ Frontend/src/Components/Item.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 const Item = () => {
   return (
-    <ul className='grid-rows-1'>
+    <ul className='grid-rows-1 text-center'>
       <li>some text</li>
       <li>some text</li>
       <li>some text</li>
       <li>some text</li>
-      <li>some text</li>
-      <li>some text</li>
-      <li>some text</li>
-
+      
     </ul>
   )
 }
--- Frontend/src/Components/Itemscontainer.jsx
+++ Frontend/src/Components/Itemscontainer.jsx
@@ -3,7 +3,10 @@ import Item from './Item'
 
 const Itemscontainer = () => {
   return (
-    <div>
+    <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 bg-[#344593] gap-6 sm:px-8 px-5 py-16'>
+      <Item/>
+      <Item/>
+      <Item/>
       <Item/>
       
     </div>
