31a3032cb92a5b6dfbe25155267a80a727d5f020
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-21 12:09:02 +0530

    update

--- Frontend/src/Components/SocialIcon.jsx
+++ Frontend/src/Components/SocialIcon.jsx
@@ -8,7 +8,7 @@ const SocialIcon = ({ Icons }) => {
         <span
           key={icon.name}
           className="p-2 cursor-pointer inline-flex items-center
-        rounded-full bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500
+        rounded-[12px] bg-gray-700 mx-1.5 text-xl hover:text-gray-100 hover:bg-teal-500
         duration-300 "
         >
           <ion-icon name={icon.name}></ion-icon>
