1ee8ffef5737d09028261bd601de9c0b83c8e779
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-21 13:09:29 +0530

    update

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     <div className='md:flex  md: justify-between md:items-center sm: px-12 px-4 bg-[#0E145A] py-7'>
       <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5 flex ml-2'><span className='text-teal-400 '><NavLink to='/Home'><img src='https://th.bing.com/th/id/OIG2..qpctzdoHBeFrDp_BRbR?pid=ImgGn' className='h-20 w-auto' alt='' /></NavLink></span >&nbsp; Code Foundry</h1>
       <div>
-        <input type='text ' placeholder='Enter your email ' className='text-white-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none bg-[#181F76] text-lg' />
-        <button className='bg-[#3A9EFD] hover:bg-cyan-600 duration-300 px-5 py-2.5 font-[popins] rounded-md text-white text-xl md:w-auto w-full'>Subscribe</button>
+        <input type='email ' placeholder='Enter your email ' className='text-white-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none bg-[#181F76] text-lg' />
+        <button  className='bg-[#3A9EFD] hover:bg-cyan-600 duration-300 px-5 py-2.5 font-[popins] rounded-md text-white text-xl md:w-auto w-full'>Submit</button>
       </div>
     </div>
     <Itemscontainer />
