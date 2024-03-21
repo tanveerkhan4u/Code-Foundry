858622d088daf37d044c9a5d8cfe7d9baaeaa2a7
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-21 19:40:36 +0530

    update

--- Frontend/src/Components/Browsestack.jsx
+++ Frontend/src/Components/Browsestack.jsx
@@ -237,8 +237,8 @@ const Browsestack = () => {
 
 
       <div className='container-fluid'>
-        <h2 className='text' style={{ marginLeft: "50px" }}>Admin Template</h2><br />
-        <p className='text' style={{ marginLeft: "50px" }}>If you are a developer and worried about your back-end UI for your project, so here we offer a <strong>highly responsive </strong> and <strong>well-designed </strong>admin dashboard template.<br />
+        <h2 className='text-3xl ml-10 font-semibold'>Admin Template</h2><br />
+        <p className='text ml-10'>If you are a developer and worried about your back-end UI for your project, so here we offer a <strong>highly responsive </strong> and <strong>well-designed </strong>admin dashboard template.<br />
 
           Admin templates are collections of web pages that are built with HTML, CSS, and JavaScript for typography, forms, buttons, navigation, and other interface components.<br />
 
@@ -246,8 +246,8 @@ const Browsestack = () => {
       </div>
       <br />
       <div className='container10'>
-        <h3 className='text' style={{ marginLeft: "50px" }}>With the help of our admin dashboard, you can build -</h3><br />
-        <div className='container bg-body-secondary' style={{ marginLeft: "50px" }}>
+        <h3 className='text-3xl font-semibold ml-12'>With the help of our admin dashboard, you can build -</h3><br />
+        <div className='container bg-body-secondary ml-11'>
           <ul className='text'>
             <li className='text' style={{ marginBottom: "10px", fontSize: "20px" }}>eCommerce Apps</li>
 
@@ -263,9 +263,9 @@ const Browsestack = () => {
       </div>
       <br />
       <div>
-        <h3 className='text' style={{ marginLeft: "50px" }}>Benefits of using an admin dashboard template</h3><br />
-        <p className='text' style={{ marginLeft: "50px" }}>The biggest benefit of adopting an admin panel template is that you may combine it with any backend framework. Such as Laravel, Django, .NET, React, Flask, Angular, Bootstrap, etc. See what are the other benefits:-</p>
-        <div className='container bg-body-secondary' style={{ marginLeft: "50px" }}>
+        <h3 className='text-3xl font-semibold ml-11'>Benefits of using an admin dashboard template</h3><br />
+        <p className='text ml-11 mb-3'>The biggest benefit of adopting an admin panel template is that you may combine it with any backend framework. Such as Laravel, Django, .NET, React, Flask, Angular, Bootstrap, etc. See what are the other benefits:-</p>
+        <div className='container bg-body-secondary ml-10'>
           <ul className='text'>
             <li className='text' style={{ marginBottom: "10px", fontSize: "20px" }}>Ready to use</li>
             <li className='text' style={{ marginBottom: "10px", fontSize: "20px" }}>Time saving</li>
@@ -276,24 +276,24 @@ const Browsestack = () => {
         </div>
       </div>
       <br />
-      <div className='container-fluid' style={{ marginLeft: "50px" }}>
-        <h2 className='text'>UI Kits</h2>
-        <p className='text'>A UI kit is a build with design elements such as a style and UI components like input forms, widgets, and navigation menus.<br />
+      <div className='container-fluid'>
+        <h2 className='text-3xl ml-8 mb-2'>UI Kits</h2>
+        <p className='text-md ml-8 mb-3'>A UI kit is a build with design elements such as a style and UI components like input forms, widgets, and navigation menus.<br/>
 
-          We make UI kits based on atomic design which provides a clear methodology for crafting design systems.<br />
+          We make UI kits based on atomic design which provides a clear methodology for crafting design systems.<br/>
 
           A simple and good-looking <strong >UI kit </strong>makes easy product design. Our UI kits are <strong>clean, well-built</strong> and <strong>less complicated.</strong></p>
       </div>
-      <div className='container-fluid' style={{ margin: "50px" }}>
-        <h2 className='text'>Why <strong className='text-primary'>CodeFoundry </strong>Template?</h2><br />
+      <div className='container-fluid' >
+        <h2 className='text-3xl ml-7 mb-3'>Why <strong className='text-primary'>CodeFoundry </strong>Template?</h2>
         <ul className='text'>
-          <li className='text' style={{ marginRight: "80px", marginBottom: "15px" }}>We develop Free and Premium Admin templates in Bootstrap, Angular, React, Vue, Laravel, CodeIgniter, etc as well as UI kits also. As never seen before we provide the best and a vast variety of options with our admin template in comparison to other admin template providers in the market.</li>
-          <li className='text' style={{ marginBottom: "15px" }}>Our templates are<strong> flexible, customizable, bug-free, and responsive.</strong> It helps you to create a stunning website with minimalist dashboards, themes, and panels. </li>
-          <li className='text' style={{ marginRight: "80px", marginBottom: "15px" }}>Each of our template designs speeds up the development process. All our dashboard panels are designed by our expert UX developer team. We created an interface with the right combination of shape, color, content layout, elements, and fonts.</li>
-          <li className='text' style={{ marginRight: "80px", marginBottom: "15px" }}>It comes with <strong> highly feature-rich pages </strong> and components with <strong> fully developer-centric code. </strong> While developing each Admin template we go through various testing & code optimization processes, for making this performance-centric backend application.</li>
-          <li className='text' style={{ marginRight: "80px", marginBottom: "15px" }}>We provided all possible prebuilt admin dashboard layouts, which gives you the best selection choice of the backend template needed for your projects. It comes with error/bug-free, well-structured, well-commented code and regularly with all the latest updated code. This saves your large amount of developing backend r application time and it is fully customizable.</li>
-          <li style={{ marginRight: "70px", marginBottom: "15px" }}>We also provide a free admin template so you can check our code quality. If you are satisfied with our quality then you move to the premium ones, which have more features and components that make your unique project easy.</li>
-          <li>You will get <strong> premium support, docs access, </strong>and  <strong>free updates </strong> based on the license you purchased.</li>
+          <li className='text ml-7 mb-2' >We develop Free and Premium Admin templates in Bootstrap, Angular, React, Vue, Laravel, CodeIgniter, etc as well as UI kits also. As never seen before we provide the best and a vast variety of options with our admin template in comparison to other admin template providers in the market.</li>
+          <li className='text ml-7 mb-2' >Our templates are<strong> flexible, customizable, bug-free, and responsive.</strong> It helps you to create a stunning website with minimalist dashboards, themes, and panels. </li>
+          <li className='text ml-7 mb-2' >Each of our template designs speeds up the development process. All our dashboard panels are designed by our expert UX developer team. We created an interface with the right combination of shape, color, content layout, elements, and fonts.</li>
+          <li className='text ml-7 mb-2'>It comes with <strong> highly feature-rich pages </strong> and components with <strong> fully developer-centric code. </strong> While developing each Admin template we go through various testing & code optimization processes, for making this performance-centric backend application.</li>
+          <li className='text ml-7 mb-2' >We provided all possible prebuilt admin dashboard layouts, which gives you the best selection choice of the backend template needed for your projects. It comes with error/bug-free, well-structured, well-commented code and regularly with all the latest updated code. This saves your large amount of developing backend r application time and it is fully customizable.</li>
+          <li className='text ml-7 mb-2'>We also provide a free admin template so you can check our code quality. If you are satisfied with our quality then you move to the premium ones, which have more features and components that make your unique project easy.</li>
+          <li className='text ml-7 mb-3'>You will get <strong> premium support, docs access, </strong>and  <strong>free updates </strong> based on the license you purchased.</li>
         </ul>
       </div>
     </div>
