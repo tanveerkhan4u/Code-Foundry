b4480a677ae092a0112ad04038d48b70f2f0ce07
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-22 20:06:50 +0530

    update

--- Frontend/src/Components/Reactdetail.jsx
+++ Frontend/src/Components/Reactdetail.jsx
@@ -6,34 +6,29 @@ const Reactdetail = () => {
   return (
     <div >
 
-      <header className= 'flex mx-auto md:flex-row py-5 flex-col min-lg-screen container-fluid  bg-[#dbedff] '>
+      <header className= 'flex mx-auto md:flex-row flex-col min-lg-screen   bg-[#dbedff] '>
 
-        <div className='basis-1/2 reactemp '>
+        <div className='basis-1/2  reactemp mt-5'>
           <motion.h2 animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} className='text-3xl ' ><strong className='text-primary'>React </strong> Admin Template</motion.h2>&nbsp;
-          <motion.p animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} className='text ' >Are you looking for a React Admin Template, For your next project? You are at the right place. We are one of the Best Admin Dashboard makers in the market. Our team develops the<strong className='text' style={{ color: "grey" }}> most advanced, highly customizable </strong> React Template. All our Admin Dashboards are fully responsive and tested on all retina devices. With  <strong className='text' style={{ color: "grey" }}> clean, bug-free and well comment code.</strong></motion.p>&nbsp;
+          <motion.p animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} className='text-gray-500 text-md ' >Are you looking for a React Admin Template, For your next project? You are at the right place. We are one of the Best Admin Dashboard makers in the market. Our team develops the<strong className='text'> most advanced, highly customizable </strong> React Template. All our Admin Dashboards are fully responsive and tested on all retina devices. With  <strong className='text' style={{ color: "grey" }}> clean, bug-free and well comment code.</strong></motion.p>&nbsp;
           <motion.div animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} >
           <div className='row'>
-            <div className='col-md-9 '>
+            <div className='col-md-9 mt-4'>
               <div className='input-group'>
                 <input type="text" className="form-control fs-5" placeholder='Search...' />
                 <button1 >Search</button1>
               </div>
             </div>
           </div>
-           
           </motion.div>
         </div>
-
-
-
         <div className='basis-1/2 reactimg '>
-          <motion.img animate={{ x: 50, y: -40 }} transition={{ ease: "easeOut", duration: 2 }} src='https://codedthemes.com/wp-content/uploads/2022/04/React-Banner.svg' className='w-100 mx-auto ' alt=''></motion.img>
+          <motion.img animate={{ x: 50, y: -60 }} transition={{ ease: "easeOut", duration: 2 }} src='https://codedthemes.com/wp-content/uploads/2022/04/React-Banner.svg' className='w-100 mx-auto ' alt=''></motion.img>
         </div>
       </header>
-    
       <br />
       <br />
-      <div className='container-fluid'>
+      <div className='container'>
         <div className='row gy-4 justify-content-between'>
           <div className='col-md-4'>
             <Link to="/Exportstack/react" style={{ textDecoration: "none" }}>
@@ -69,9 +64,14 @@ const Reactdetail = () => {
       </div>
       <br />
       <br />
+    
+           
+
+
+
       <div className='container-fluid'>
-        <h2 className='text' >React Admin Templates</h2><br />
-        <p className='text' style={{ marginLeft: "50px" }}>Well, <strong> React </strong> is a free and open-source front-end JavaScript library for building UI. React dashboard Template are collection of web pages developed with <strong> HTML, CSS, and JavaScript.</strong><br /><br />
+        <h2 className='text-3xl font-semibold ml-8' >React Admin Templates</h2><br />
+        <p className='text ml-8'>Well, <strong> React </strong> is a free and open-source front-end JavaScript library for building UI. React dashboard Template are collection of web pages developed with <strong> HTML, CSS, and JavaScript.</strong><br /><br />
 
           Our admin dashboard are built with <strong> Next.js and MUI  </strong>to provide the best developer experience with all the features you need for production. A ready template makes your developing task easy because you don’t wait for the designer to design for you. We develop <strong>responsive and fully customized  </strong>admin templates that will help you to create responsive web apps in no time.<br /><br />
 
@@ -81,15 +81,15 @@ const Reactdetail = () => {
       </div>
       <br />
       <div className='container-fluid'>
-        <h2 className='text' style={{ marginLeft: "50px" }}>What does it Include?</h2><br />
-        <p className='text' style={{ marginLeft: "50px" }}> A developer with the knowledge of HTML, CSS, & JavaScript can easily use this without any hesitation because it's developer-friendly. The template comes with <strong> well-written documentation </strong> that guides every step from installation to run. Furthermore, every template also includes a starter kit that will help you start your project rather than removing unnecessary stuff.<br /><br />
+        <h2 className='text-3xl ml-8 font-semibold'>What does it Include?</h2><br />
+        <p className='text  ml-8'> A developer with the knowledge of HTML, CSS, & JavaScript can easily use this without any hesitation because it's developer-friendly. The template comes with <strong> well-written documentation </strong> that guides every step from installation to run. Furthermore, every template also includes a starter kit that will help you start your project rather than removing unnecessary stuff.<br /><br />
 
           If you are still confused then you may try our <strong> Free react admin template, </strong> so you can check our code quality. After that, you move to the premium ones which include more features that are necessary for your new project.</p>
       </div>
       <br />
-      <div className='container-fluid'>
-        <h3 className='text' style={{ marginLeft: "50px" }}>Some of our Features:</h3><br />
-        <div className='container bg-light bg-gradient' style={{ marginLeft: "50px" }}>
+      <div className='container'>
+        <h3 className='text-2xl font-semibold ' >Some of our Features:</h3><br />
+        <div className='container bg-light bg-gradient mb-4' >
           <ul className='text'>
             <li className='text' style={{ marginBottom: "10px", fontSize: "20px" }}> Figma Dashboard pages</li>
 
