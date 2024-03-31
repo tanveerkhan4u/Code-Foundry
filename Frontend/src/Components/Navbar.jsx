1d874ff41080be6085f0cc768f541e9fd92dd4c7
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-03-31 20:40:14 +0530

    update

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -194,6 +194,18 @@ const Navbar = () => {
           >
             Feedback
           </NavLink>
+          <NavLink
+            to="/Login"
+            className="text-gray-300 hover:bg-gray-900 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to="/Signup"
+            className="text-gray-300 hover:bg-gray-900 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+          >
+            Signup
+          </NavLink>
         </div>
       </div>
       <ul>
