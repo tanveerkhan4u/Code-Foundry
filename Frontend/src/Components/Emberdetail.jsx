a8026a74d112b6b3d1b2d4c494a1498190f1560a
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-04-08 19:47:20 +0530

    updated

--- Frontend/src/Components/Emberdetail.jsx
+++ Frontend/src/Components/Emberdetail.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom' 
+import Card from './Card';
+
+const emberData = [
+  {
+    name: 'Ember',
+    image: 'https://codedthemes.com/wp-content/uploads/edd/2022/05/Materially-Free-Reactjs-Admin-Template.webp',
+    link: '/Exportstack/ember',
+    description : 'Mantis MUI Ember Dashboard Template'
+  },
+  {
+    name: 'Ember',
+    image: 'https://codedthemes.com/wp-content/uploads/edd/2022/05/Datta-Able-React-Admin-Template.webp',
+    link: '/Exportstack/ember',
+    description : 'Berry MUI Ember Dashboard Template'
+  },
+  {
+    name: 'Ember',
+    image: 'https://codedthemes.com/wp-content/uploads/edd/2022/05/Gradient-Able-Reactjs-Admin-Dashboard.webp',
+    link: '/Exportstack/ember',
+    description : 'Materially Ember Admin Dashboard'
+  },
+];
+
+
+
+
 
 const Emberdetail = () => {
   return (
     <div>
       
-      <header className='row  d-flex ' style={{ backgroundColor: "#dbedff" }}>
-        <div className='col-md-6 py-5 '>
-          <motion.h2 animate={{ x: 100 }} transition={{ ease: "easeOut", duration: 2 }} className='text'><strong className='text-primary'>Ember </strong></motion.h2>&nbsp;
-          <motion.p animate={{ x: 100 }} transition={{ ease: "easeOut", duration: 2 }} className='text ember' style={{color: "grey" }}>Ember.js is a productive, battle-tested JavaScript framework for building modern web applications. It includes everything you need to build rich UIs that work on any device.
+      <header className='flex mx-auto md:flex-row flex-col min-lg-screen   bg-[#dbedff]'>
+        <div className='basis-1/2 mt-5 ember '>
+          <motion.h2 animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} className='text-3xl '><strong className='text-primary'>Ember </strong></motion.h2>&nbsp;
+          <motion.p animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} className='text-gray-500 text-md '>Ember.js is a productive, battle-tested JavaScript framework for building modern web applications. It includes everything you need to build rich UIs that work on any device.
 
           </motion.p>&nbsp;
           
-          <motion.div animate={{ x: 100 }} transition={{ ease: "easeOut", duration: 2 }} >
+          <motion.div animate={{ x: 50 }} transition={{ ease: "easeOut", duration: 2 }} >
           <div className='row'>
-            <div className='col-md-9 embersearch'>
+            <div className='col-md-9 '>
               <div className='input-group'>
                 <input type="text" className="form-control fs-5" placeholder='Search...' />
                 <button1 >Search</button1>
@@ -24,48 +50,25 @@ const Emberdetail = () => {
           </div>
           </motion.div>
         </div>
-      <div className='col-md-2 p-2 mt-5 ms-5'>
-        <motion.img animate={{ x: 50, y: -20 }} transition={{ ease: "easeOut", duration: 2 }} src='https://blog.emberjs.com/images/logos/e-icon.png' className='img-fluid mx-auto tiger' alt=''></motion.img>
+      <div className='basis-1/2  mt-5'>
+        <motion.img animate={{ x: 50, y: -20 }} transition={{ ease: "easeOut", duration: 2 }} src='https://blog.emberjs.com/images/logos/e-icon.png' className='img-fluid mx-auto tiger  w-96 h-70' alt=''></motion.img>
       </div>
       </header>
-      <br />
-      <br />
-      <div className='conatiner-fluid'>
-        <div className="row gy-4">
-          <div className="col-md-4">
-      <Link to="/Exportstack/ember" style={{textDecoration:"none"}}>
-      <div className="card" >
-          <img src="https://raw.githubusercontent.com/huxinghai1988/ember-adminlte-theme/master/preview.png"  className="card-img-top img-fluid mx-auto" alt="..." />
-          <div className="card-body">
-            <h5 className='text' >Ember</h5>
-          </div>
-        </div>
-        </Link>
-        </div>
+      <div className='container'>
+        <div className='row '>
+          {
+            emberData.map(data => (
+              <div className="col-md-4 ">
 
-        <div className="col-md-4">
-      <Link to="/Exportstack/ember" style={{textDecoration:"none"}}>
-      <div className="card" >
-          <img src="https://raw.githubusercontent.com/huxinghai1988/ember-adminlte-theme/master/preview.png"  className="card-img-top img-fluid mx-auto" alt="..." />
-          <div className="card-body">
-            <h5 className='text' >Ember</h5>
-          </div>
-        </div>
-        </Link>
-        </div>
-        <div className="col-md-4">
-        <Link to="/Exportstack/ember" style={{textDecoration:"none"}}>
-        <div className="card">
-          <img src="https://5.imimg.com/data5/SELLER/Default/2021/6/NZ/HL/XM/4216320/hospital-management-software-500x500.png" className="card-img-top img-fluid mx-auto" alt="..." />
-          <div className="card-body">
-            <h5 className='text' >Ember</h5>
-          </div>
-        </div>
-        </Link>
-        </div>
+                <Link to={data.link} className='text-decoration-none'>
+                  <Card product={data}  />
+                </Link>
+
+              </div>
+            ))
+          }
         </div>
       </div>
-      <br/><br/>
 
     </div>
   )
